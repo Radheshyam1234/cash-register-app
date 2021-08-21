9748eabfb74260fc725e29db73b3213fd1ae9d48
Radheshyam1234 <88738458+Radheshyam1234@users.noreply.github.com>
2021-08-21 08:47:57 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 var billAmount = document.querySelector("#billamount");
 var cashgiven = document.querySelector("#cashgiven");
-var button = document.querySelector("button");
+var calculateButton = document.querySelector("#btn-calculate");
+var nextButton = document.querySelector("#btn-next");
 var output = document.querySelector("#outputTxt");
 var outputBox = document.querySelector("#output_div");
+var cashgivenDiv=document.querySelector(".cashgiven_div");
+ 
 
 
 var Avalcurrencies = [2000, 1000, 500, 200, 100, 50, 20, 10, 5, 1];
+
+
+
+function showCashgivenInput(){
+  var bill = parseInt(billAmount.value);
+  if(!bill){
+    output.innerText = "Enter the bill amount first!";
+  }
+  else{
+    cashgivenDiv.style.display="block"
+    nextButton.style.display="none"
+    
+  }
+  
+}
 
 function calculate() {
    
@@ -13,16 +31,18 @@ function calculate() {
   var given = parseInt(cashgiven.value);
   var balance = given - bill;
   
- if(!bill||!given){
-  output.innerText = "Please enter both the fields";
- }
-
-
- else {if (bill == given) {
+  if(!bill||!given){
+    output.innerText = "Please enter both the amounts";
+  }
+else
+    {
+      if (bill == given) {
     output.innerText = "Zero Balance";
-  } else if (bill > given) {
+     } 
+  else if (bill > given) {
     output.innerText = "You have not given a sufficient amount!";
-  } else {
+    }
+   else {
    
     output.innerText = " Available Balance = ₹ " + balance + "\n"+"\n";
     Avalcurrencies.map((money) => {
@@ -33,7 +53,10 @@ function calculate() {
       }
     });
   
-  }}
+  }
+}
 }
 
-button.addEventListener("click", calculate);
+calculateButton.addEventListener("click", calculate);
+
+nextButton.addEventListener("click", showCashgivenInput);
